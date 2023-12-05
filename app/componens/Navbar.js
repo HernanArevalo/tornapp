4e6a553ce6cd27fd1bf51fddce8b3cb8382c8b66
Hernan Arevalo <hernanarevalo16@gmail.com>
2023-12-04 22:46:49 -0300

    Start routes tree

---
+++ app/componens/Navbar.js
@@ -0,0 +1,9 @@
+import styles from './Navbar.module.css'
+
+export default function Navbar () {
+  return (
+    <nav className={styles.navbar}>
+      <h1>Navbar</h1>
+    </nav>
+  )
+}
---
+++ app/tournaments/page.jsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Tournaments () {
+  return (
+    <>
+      <Head>
+        <title>Tournaments</title>
+      </Head>
+
+      <div>
+        <h1>Tournaments</h1>
+      </div>
+
+    </>
+  )
+}
