4eae6e16e9b01dd9f13f948606c8e4ffe6b911b9
Hernan Arevalo <hernanarevalo16@gmail.com>
2024-01-26 18:03:47 -0300

    Style tournament section in TeamPage

---
+++ app/teams/[id]/tournaments/components/tournamentCard.jsx
@@ -0,0 +1,14 @@
+
+
+export default function tournamentCard (tournament) {
+
+    console.log('asd');
+
+return (
+<article className={styles.tournament} key={tournament.id}>
+    <h4>{tournament.name}</h4>
+    <span> tournament.year </span>
+
+</article>
+)
+}
--- app/teams/[id]/tournaments/page.jsx
+++ app/teams/[id]/tournaments/page.jsx
@@ -1,16 +1,27 @@
-import Head from 'next/head'
+import styles from './tournaments.module.css'
+import { teams } from '../../../../data/teams.data.json'
+import tournamentCard from './components/tournamentCard'
 
 export default function TournamentPAge() {
+
+    const team = teams.find(element => element.id == "12345678")
+	const lastTournament = team.tournaments[team.tournaments.length -1]
+
 
 
 return (
 <>
+<h1>TORNEOS</h1>
+{ team.tournaments.map(tournament => (
+    <article className={styles.tournament} key={tournament.id}>
+    <h4>{tournament.name}</h4>
+    <span> tournament.year </span>
 
-<div>
-    <h1>TORNEOS</h1>
-    TournamentPage
-</div>
+</article>    
+))
 
+}
 </>
+
 )
 }
