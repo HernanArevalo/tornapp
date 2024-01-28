9d7d3ed3dd6762394b77ffa5c4f34c6c8c5e1e6f
Hernan Arevalo <hernanarevalo16@gmail.com>
2024-01-28 20:10:41 -0300

    Update team/tournaments page style

--- app/teams/[id]/tournaments/page.jsx
+++ app/teams/[id]/tournaments/page.jsx
@@ -1,6 +1,7 @@
 import styles from './tournaments.module.css'
 import { teams } from '../../../../data/teams.data.json'
 import tournamentCard from './components/tournamentCard'
+import Link from 'next/link'
 
 export default function TournamentPAge() {
 
@@ -13,15 +14,30 @@ return (
 <>
 <h1>TORNEOS</h1>
 { team.tournaments.map(tournament => (
-    <article className={styles.tournament} key={tournament.id}>
-    <h4>{tournament.name}</h4>
-    <span> tournament.year </span>
-
+<Link href={`/tournaments/${tournament.id}`} key={tournament.id}>
+<article className={styles.tournament}>
+    <div className={styles.tournamentTop}>
+        <h4 className={styles.h4}>{tournament.name}</h4>
+        { tournament.playing ?
+        <div className={styles.playing}>
+            PLAYING
+        </div>
+        :
+        <div className={styles.finished}>
+            FINISHED
+        </div>
+        }
+    </div>    
+    <span> {tournament.year} </span>
+    <div className={styles.tournamentData}>
+				Posición: 
+                <span className={styles.position}>{ tournament.position }°</span>
+	</div>
 </article>    
+</Link>
 ))
 
 }
 </>
-
 )
 }
