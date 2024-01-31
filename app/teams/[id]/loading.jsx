71ac5c3e0e91d369f1f7f7e2610a9d7db9177714
Hernan Arevalo <hernanarevalo16@gmail.com>
2024-01-31 00:11:18 -0300

    Create loading page

--- app/teams/[id]/loading.jsx
+++ app/teams/[id]/loading.jsx
@@ -3,7 +3,7 @@ import './loading.css'
 export default function LoadingPage () {
 return (
 <div className="loadingcontainer">
-    <div class="loadership_GHMVN"><div></div><div></div><div></div><div></div></div>
+<div class="loadership_ZHTYC"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
 </div>
 )
 }
