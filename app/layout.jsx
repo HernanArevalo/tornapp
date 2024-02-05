b97296886e0c6c79e47ba00091f69c79c69055ba
Hernan Arevalo <hernanarevalo16@gmail.com>
2024-02-05 11:55:08 -0300

    Change components route

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,6 @@
 import { Kanit } from 'next/font/google'
 import '@/styles/globals.css'
-import Navbar from '@/components/Navbar'
+import Navbar from 'components/Navbar'
 
 const inter = Kanit({ 
   subsets: ['latin'],
