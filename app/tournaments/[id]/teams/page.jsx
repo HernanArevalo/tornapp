3f153abcd5894a48d59e2948cf977bf6881c9d33
Hernan Arevalo <hernanarevalo16@gmail.com>
2024-02-05 10:50:27 -0300

    Style teams tournament page

--- app/tournaments/[id]/teams/page.jsx
+++ app/tournaments/[id]/teams/page.jsx
@@ -1,6 +1,7 @@
 import styles from './teams.module.css'
 import tournamentsData from '../../../../data/tournaments.data.json';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function TeamsPage () {
 
@@ -11,19 +12,50 @@ return (
 <h1>EQUIPOS</h1>
 <div className={ styles.teams_list }>
     { tournament.teams.map(team => (
-        <div className={styles.team_container} key={team.id}>
-        <div className={ styles.teamData }>
-            <Image src={team.team_badge} alt="team logo" width={64} height={64}/>
-            <h3 className={ styles.h3 }> { team.name } </h3>
-        </div>
-        <div className={ styles.teamStats }>
-            <span className={ styles.h3 }> { team.games } </span>
-            <span className={ styles.h3 }> { team.games_won } </span>
-            <span className={ styles.h3 }> { team.games_draw } </span>
-            <span className={ styles.h3 }> { team.games_lost } </span>
-        </div>
-        </div>
+        <Link href={`/teams/${team.team_id}`} key={team.id} className={styles.teamLink}>
+        <article className={styles.team_container}>
+            <h3 className={ styles.team_name }>{ team.name }</h3>
+            <div className={ styles.teamData }>
+                <Image src={team.team_badge} alt="team logo" width={64} height={64}/>
+                <div className={ styles.teamStats }>
+                    <div className={ styles.games }>
+                        <span className={ styles.stat_title }>PJ </span>
+                        <span className={ styles.stat }>{ team.games } </span>
+                    </div>
+                    <div className={ styles.games }>
+                        <span className={ styles.stat_title }>PG</span>
+                        <span className={ styles.stat }>{ team.games_won }</span>
+                    </div>
+                    <div className={ styles.games }>
+                        <span className={ styles.stat_title }>PE</span>
+                        <span className={ styles.stat }>{ team.games_draw }</span>
+                    </div>
+                    <div className={ styles.games }>
+                        <span className={ styles.stat_title }>PP</span>
+                        <span className={ styles.stat }>{ team.games_lost }</span>
+                    </div>
+                </div>
+                <div className={styles.pointsStats}>
+                    <div className={styles.pointStatsChildren}>
+                        <div className={styles.pointStatsTitle}>Puntos obtenidos:</div>
+                        <div className={styles.data}>
+                            <div>{team.games_won*3+team.games_draw}</div>
+                            <div>/</div>
+                            <div>{team.games*3}</div>
+                        </div>
+                    </div>
+                    <div className={styles.pointStatsChildren}>
+                        <div className={styles.pointStatsTitle}>Porcentaje victoria:</div>
+                        <div className={styles.data}>
+                            <div>{parseFloat( (team.games_won*3+team.games_draw)*100/(team.games*3)).toFixed(2)}</div>
+                            <div>%</div>
+                        </div>
+                    </div>
+		        </div>
 
+            </div>
+        </article>
+        </Link>
     ))
 
     }
